afbdc5985a31e564fdf1a2e217aaa9d943e4fc0f
drrosa simidat <drrosa@ujaen.es>
2026-06-08 14:37:56 +0200

    First commit

---
+++ R/utils.R
@@ -0,0 +1,23 @@
+
+#' Check if GPU is available
+#'
+#' @returns Command line message indicating whether GPU is available or not.
+#' @export
+#' @importFrom torch cuda_is_available
+#'
+#' @examples
+#' check_if_gpu_available()
+check_if_gpu_available <- function() {
+
+  if(cuda_is_available()){
+    print("Graphic card found :D")
+  } else {
+    print("Graphic card not found D:")
+  }
+
+}
+
+
+
+
+
